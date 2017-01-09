8d3aee780a3f10f0bff66dface6d94ab10b64d0b
Caleb Wells <calebcwells@gmail.com>
2017-01-09 13:39:32 -0600

    Design updates.

--- src/Client/app/home/home.component.ts
+++ src/Client/app/home/home.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent {
+    currentYear: string;
+
+    constructor(http: Http) {
+        http.get('/api/Contact/CurrentYear').subscribe(result => {
+            this.currentYear = result.json();
+        })
+    }
+}
---
+++ src/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataWells.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        [HttpGet("[action]")]
+        public int CurrentYear()
+        {
+            return DateTime.Now.Year;
+        }
+    }
+}
