f2835b8fc0cc05a78c22489ec25886958a8314e6
Caleb Wells <calebcwells@gmail.com>
2017-01-12 12:19:13 -0600

    Added hash routes to fix zone.js error thrown when clicking nav links.

--- src/Client/app/app.routes.ts
+++ src/Client/app/app.routes.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'page-top', redirectTo: 'home' },
+  { path: 'about', redirectTo: 'home' },
+  { path: 'skills', redirectTo: 'home' },
+  { path: 'contact', redirectTo: 'home' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes,
-    {
-        useHash: true
-    })],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
