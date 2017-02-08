02d39f444b37fdf69a0b70ec0d1bad5d3512d5a4
Caleb Wells <calebcwells@gmail.com>
2017-02-08 10:52:25 -0600

    Email functionality completed using SparkPost

--- src/Controllers/ContactController.cs
+++ src/Controllers/ContactController.cs
@@ -15,20 +15,21 @@ namespace DataWells.Controllers
         public void Post([FromBody] Contact contact)
         {
             if (contact == null || !ModelState.IsValid) return;
+            var completeMessage = $"Name: {contact.Name} \rPhone: {contact.Phone} \rEmail: {contact.Email} \rMessage: {contact.Message} ";
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress(contact.Name, String.IsNullOrEmpty(contact.Email) ? contact.Email : "calebcwells@gmail.com"));
+            emailMessage.From.Add(new MailboxAddress("DataWells", "mail@mail.datawells.us"));
             emailMessage.To.Add(new MailboxAddress("Caleb Wells", "calebcwells@gmail.com"));
             emailMessage.Subject = "[DataWells] - Contact from " + contact.Name;
-            emailMessage.Body = new TextPart("plain") { Text = contact.Message };
+            emailMessage.Body = new TextPart("plain") { Text = completeMessage };
 
-            //using (var client = new SmtpClient())
-            //{
-            //    client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTlsWhenAvailable);
-            //    client.AuthenticationMechanisms.Remove("XOAUTH2");
-            //    client.Send(emailMessage);
-            //    client.Disconnect(true);
-            //}
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.sparkpostmail.com", 587, SecureSocketOptions.StartTls);
+                client.Authenticate("SMTP_Injection", "API_KEY");
+                client.Send(emailMessage);
+                client.Disconnect(true);
+            }
         }
     }
 }
